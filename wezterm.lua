622ea6e9e86d8c3c902fbeecd189d76f1e6bdbe1
atici <aticiarda@gmail.com>
2026-07-26 06:02:44 +0300

    Updated keybinds and organized the file

--- wezterm.lua
+++ wezterm.lua
@@ -8,7 +8,7 @@ local config = {
         XDG_DATA_HOME   = "C:/dev",
     },
     color_scheme = "Tokyo Night Moon",
-    default_prog = { "pwsh" },
+    default_prog = { "pwsh", "-NoLogo" },
     enable_tab_bar = true,
     use_fancy_tab_bar = false,
     tab_bar_at_bottom = true,
@@ -16,95 +16,49 @@ local config = {
     font_size = 9.8,
     enable_csi_u_key_encoding = true,
     allow_win32_input_mode = false,
-    window_padding = {
-        left = "0",
-        right = "0",
-        top = "0",
-        bottom = "0",
-    },
+    window_padding = { left = "0", right = "0", top = "0", bottom = "0", },
     keys = {
+        -- Config
+        { key = "c",          mods = "CTRL|ALT", action = act.SpawnCommandInNewTab({ args = { "nvim", wezterm.config_dir .. "/wezterm.lua" } }), },
+        { key = 'x',          mods = "ALT",      action = act.ActivateCopyMode },
+        -- Tab Control
+        { key = "q",          mods = "ALT",      action = act.CloseCurrentPane({ confirm = false }), },
+        { key = "t",          mods = "ALT",      action = act.SpawnTab("CurrentPaneDomain"), },
+        -- Splits
+        { key = "s",          mods = "ALT",      action = act.SplitHorizontal, },
+        { key = "v",          mods = "ALT",      action = act.SplitVertical, },
+        -- Split Navigation
+        { key = "j",          mods = "ALT",      action = act.ActivatePaneDirection("Down"), },
+        { key = "h",          mods = "ALT",      action = act.ActivatePaneDirection("Left"), },
+        { key = "l",          mods = "ALT",      action = act.ActivatePaneDirection("Right"), },
+        { key = "k",          mods = "ALT",      action = act.ActivatePaneDirection("Up"), },
+        { key = "DownArrow",  mods = "ALT",      action = act.AdjustPaneSize({ "Down", 10 }), },
+        { key = "LeftArrow",  mods = "ALT",      action = act.AdjustPaneSize({ "Left", 10 }), },
+        { key = "RightArrow", mods = "ALT",      action = act.AdjustPaneSize({ "Right", 10 }), },
+        { key = "UpArrow",    mods = "ALT",      action = act.AdjustPaneSize({ "Up", 10 }), },
+        -- Scrolling
+        { key = "u",          mods = "ALT",      action = act.ScrollByLine(-10), },
+        { key = "d",          mods = "ALT",      action = act.ScrollByLine(10), },
+        { key = "PageUp",     mods = "",         action = act.ScrollByPage(-1), },
+        { key = "PageDown",   mods = "",         action = act.ScrollByPage(1), },
+        -- Tab Navigation
+        { key = "o",          mods = "ALT",      action = act.ActivateLastTab },
+        { key = "[",          mods = "ALT",      action = act.ActivateTabRelative(-1) },
+        { key = "]",          mods = "ALT",      action = act.ActivateTabRelative(1) },
+        { key = "[",          mods = "CTRL|ALT", action = act.MoveTabRelative(-1) },
+        { key = "]",          mods = "CTRL|ALT", action = act.MoveTabRelative(1) },
+        -- Tab Renaming
         {
-            action = wezterm.action.ActivatePaneDirection("Down"),
-            key = "j",
+            key = "r",
             mods = "ALT",
-        },
-        {
-            action = wezterm.action.ActivatePaneDirection("Left"),
-            key = "h",
-            mods = "ALT",
-        },
-        {
-            action = wezterm.action.ActivatePaneDirection("Right"),
-            key = "l",
-            mods = "ALT",
-        },
-        {
-            action = wezterm.action.ActivatePaneDirection("Up"),
-            key = "k",
-            mods = "ALT",
-        },
-        {
-            action = wezterm.action.AdjustPaneSize({ "Down", 10 }),
-            key = "DownArrow",
-            mods = "ALT|SHIFT",
-        },
-        {
-            action = wezterm.action.AdjustPaneSize({ "Left", 10 }),
-            key = "LeftArrow",
-            mods = "ALT|SHIFT",
-        },
-        {
-            action = wezterm.action.AdjustPaneSize({ "Right", 10 }),
-            key = "RightArrow",
-            mods = "ALT|SHIFT",
-        },
-        {
-            action = wezterm.action.AdjustPaneSize({ "Up", 10 }),
-            key = "UpArrow",
-            mods = "ALT|SHIFT",
-        },
-        {
-            action = wezterm.action.CloseCurrentPane({ confirm = false }),
-            key = "q",
-            mods = "ALT",
-        },
-        {
-            action = wezterm.action.ScrollByLine(-10),
-            key = "UpArrow",
-            mods = "SHIFT",
-        },
-        {
-            action = wezterm.action.ScrollByLine(10),
-            key = "DownArrow",
-            mods = "SHIFT",
-        },
-        {
-            action = wezterm.action.ScrollByPage(-1),
-            key = "PageUp",
-        },
-        {
-            action = wezterm.action.ScrollByPage(1),
-            key = "PageDown",
-        },
-        {
-            action = wezterm.action.SpawnCommandInNewTab({ args = { "nvim", wezterm.config_dir .. "/wezterm.lua" } }),
-            key = ",",
-            mods = "CTRL|ALT",
-        },
-        {
-            action = wezterm.action.SpawnTab("CurrentPaneDomain"),
-            key = "t",
-            mods = "ALT",
-        },
-        {
-            action = wezterm.action.SplitHorizontal,
-            key = "RightArrow",
-            mods = "CTRL|ALT",
-        },
-        {
-            action = wezterm.action.SplitVertical,
-            key = "DownArrow",
-            mods = "CTRL|ALT",
+            action = act.PromptInputLine {
+                description = "Enter new name for tab",
+                action = wezterm.action_callback(function(window, pane, line)
+                    if line and line ~= "" then
+                        window:active_tab():set_title(line)
+                    end
+                end),
+            },
         },
     },
     max_fps = 240,
@@ -115,8 +69,17 @@ for i = 1, 9 do
     -- ALT + number to activate that tab
     table.insert(config.keys, {
         key = tostring(i),
-        mods = 'ALT',
+        mods = "ALT",
         action = act.ActivateTab(i - 1),
+    })
+end
+
+-- CTRL+ALT + number to move tab to that position
+for i = 1, 9 do
+    table.insert(config.keys, {
+        key = tostring(i),
+        mods = "CTRL|ALT",
+        action = wezterm.action.MoveTab(i - 1),
     })
 end
 
